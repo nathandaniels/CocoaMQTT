ea08c89bb53e57862924a8f707d7a16723fd059d
nathandaniels <nathandaniels@mac.com>
2025-02-13 22:17:23 +1030

    initial

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaMQTT",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
+        .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", .exact("1.0.8")),
+        .package(url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.4")),
+    ],
+    targets: [
+        .target(name: "CocoaMQTT",
+                dependencies: [ "MqttCocoaAsyncSocket" ],
+                path: "Source",
+                exclude: ["CocoaMQTTWebSocket.swift"],
+                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
+        .target(name: "CocoaMQTTWebSocket",
+                dependencies: [ "CocoaMQTT", "Starscream" ],
+                path: "Source",
+                sources: ["CocoaMQTTWebSocket.swift"],
+                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
+        .testTarget(name: "CocoaMQTTTests",
+                    dependencies: [ "CocoaMQTT", "CocoaMQTTWebSocket" ],
+                    path: "CocoaMQTTTests",
+                    swiftSettings: [ .define("IS_SWIFT_PACKAGE")])
+    ]
+)
